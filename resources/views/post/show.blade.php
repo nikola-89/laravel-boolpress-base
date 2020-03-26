092ac4a867d67db173ab0d685469fb5405b75585
Nicola <nicola.ba1989@gmail.com>
2020-03-26 08:58:16 +0100

    imp

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.layout')
 
 @section('main')
-	<div class="container">
-		<div class="row">
-			<div class="col-xl-12">
-				<div class="post m-5">
-					<div class="card">
-						<img class="card-img-top" src="{{$post->img}}" alt="{{$post->title}}">
-						<div class="card-body">
-							<h5 class="card-title">{{$post->title}}</h5>
-							<p class="card-text">{{$post->text}}</p>
-							<p class="card-text">Fonte: <a href="{{$post->original_source}}">{{$post->original_source}}</a></p>
-						</div>
-						<div class="card-footer text-muted">
-							<p class="card-text">{{$post->author}} in {{$post->category}} il {{$post->created_at}}</p>
-						</div>
+<div class="container mt-5">
+	<div class="row">
+		<div class="col-xl-12">
+			<div class="home-btn">
+				<a href="{{route('post.index')}}"><button class="btn btn-secondary btn-lg btn-block">Homepage</button></a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container mt-5">
+	<div class="row">
+		<div class="col-xl-12">
+			<div class="post">
+				<div class="card">
+					<img class="card-img-top" src="{{$post->img}}" alt="{{$post->title}}">
+					<div class="card-body">
+						<h5 class="card-title">{{$post->title}}</h5>
+						<p class="card-text">{{$post->text}}</p>
+						<p class="card-text">Fonte: <a href="{{$post->original_source}}">{{$post->original_source}}</a></p>
+					</div>
+					<div class="card-footer text-muted">
+						<p class="card-text">{{$post->author}} in {{$post->category}} il {{$post->created_at}}</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 @endsection
